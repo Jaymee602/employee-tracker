0c26a80ed9f3eca626f2ea8cb188810718020854
jam <jaymee602@gmail.com>
2022-09-02 19:20:27 -0700

    updated connection w/ correct db, added main menu & viewDepartments

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql2');
 
 // Connect to the database
-const db = mysql.createConnection(
+const db = mysql.createConnection (
     {
         host: 'localHost',
         // Your MySQL username,
         user: 'root',
         // Your MySQL password
-        password: '',
-        database: 'election'
+        password: 'BibbityWibbity?!4',
+        database: 'company'
     },
-    console.log('Connected to the election database.')
+    console.log('Connected to the company database.')
 ); 
 
 module.exports = db;
