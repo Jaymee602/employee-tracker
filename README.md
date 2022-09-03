d33e72f5b2e70d395a75f2ac313abae6d60beec6
jam <jaymee602@gmail.com>
2022-09-02 20:46:57 -0700

    readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Employee Tracker
+
+## Purpose
+Keep track of your wonderful team of employees!
+
+## Built with
+* JavaScript
+* Inquirer
+* MySQL2
+* Console.table
+
+## How To Use (Video Walk-Through)
+https://watch.screencastify.com/v/2Gd17cKlSvgRo7jkjVmd
--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection (
         // Your MySQL username,
         user: 'root',
         // Your MySQL password
-        password: 'BibbityWibbity?!4',
+        password: '',
         database: 'company'
     },
     console.log('Connected to the company database.')
